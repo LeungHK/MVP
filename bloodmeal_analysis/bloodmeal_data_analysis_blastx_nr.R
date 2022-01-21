0e6cb21c9e4fb573c24ba4ee2e31dab7eae5ac36
LeungHK <89854896+LeungHK@users.noreply.github.com>
2022-01-21 19:32:35 +0800

    Add files via upload

---
+++ bloodmeal_analysis/bloodmeal_data_analysis_blastx_nr.R
@@ -0,0 +1,33 @@
+getwd()
+setwd("D:/Job 2021/yunshi project")
+
+mydata <- read.csv(file = 'bloodmeal.contigs.blastx.nr.fmt6', sep = '\t', header = FALSE)
+colnames(mydata) <- c("qseqid", "sseqid", "evalue", "pident", "staxid", "ssciname",
+                      "scomname", "stitle", "sskingdom", "salltitles")
+
+mycolumn <- table(mydata$scomname)
+
+df2 <- subset(mydata, scomname %in% names(mycolumn[mycolumn > 3000]))
+
+freqtable <- sort(table(df2$scomname), increasing=T)
+
+xlim <- c(0, 1.25*max(freqtable))
+par(mar=c(5, 15, 3, 1))
+xx <- barplot(freqtable, axisnames = TRUE, horiz = TRUE, las=2, xlim= xlim, 
+              col = rainbow(20))
+
+###labels: https://stackoverflow.com/questions/12481430/how-to-display-the-frequency-at-the-top-of-each-factor-in-a-barplot-in-r
+text(y = xx, x = freqtable, label = freqtable, pos = 4, cex = 0.8, col = "red")
+
+
+##Get JPEG of plot
+jpeg(paste0("Plot_of_bloodmeal_batson_158_blastx_nr.fmt6.jpg"), width = 1920, height = 1080)
+xlim <- c(0, 1.15*max(freqtable))
+par(mar=c(10, 45, 3, 1))
+xx <- barplot(freqtable, axisnames = TRUE, horiz = TRUE, las=2, xlim= xlim, 
+              col = rainbow(69), cex.axis=3, cex.names=3)
+
+###labels: https://stackoverflow.com/questions/12481430/how-to-display-the-frequency-at-the-top-of-each-factor-in-a-barplot-in-r
+text(y = xx, x = freqtable, label = freqtable, pos = 4, cex = 3, col = "red")
+
+dev.off()
