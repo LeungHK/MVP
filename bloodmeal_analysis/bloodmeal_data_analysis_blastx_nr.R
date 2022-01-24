52f12825bf8ad0f60b42a7dbac9969fc77b87aa1
LeungHK <89854896+LeungHK@users.noreply.github.com>
2022-01-24 18:33:30 +0800

    Update bloodmeal_data_analysis_blastx_nr.R

--- bloodmeal_analysis/bloodmeal_data_analysis_blastx_nr.R
+++ bloodmeal_analysis/bloodmeal_data_analysis_blastx_nr.R
@@ -1,12 +1,12 @@
 setwd()
 
-mydata <- read.csv(file = 'bloodmeal.contigs.blastx.nr.fmt6', sep = '\t', header = FALSE)
+mydata <- read.csv(file = 'bestHits_bloodmeal.contigs.blastx.nr.fmt6', sep = '\t', header = FALSE)
 colnames(mydata) <- c("qseqid", "sseqid", "evalue", "pident", "staxid", "ssciname",
                       "scomname", "stitle", "sskingdom", "salltitles")
 
 mycolumn <- table(mydata$scomname)
 
-df2 <- subset(mydata, scomname %in% names(mycolumn[mycolumn > 3000]))
+df2 <- subset(mydata, scomname %in% names(mycolumn[mycolumn > 7]))
 
 freqtable <- sort(table(df2$scomname), increasing=T)
 
@@ -20,7 +20,7 @@ text(y = xx, x = freqtable, label = freqtable, pos = 4, cex = 0.8, col = "red")
 
 
 ##Get JPEG of plot
-jpeg(paste0("Plot_of_bloodmeal_batson_158_blastx_nr.fmt6.jpg"), width = 1920, height = 1080)
+jpeg(paste0("Plot_of_bestHits_bloodmeal_batson_158_blastx_nr.fmt6.jpg"), width = 1920, height = 1080)
 xlim <- c(0, 1.15*max(freqtable))
 par(mar=c(10, 45, 3, 1))
 xx <- barplot(freqtable, axisnames = TRUE, horiz = TRUE, las=2, xlim= xlim, 
